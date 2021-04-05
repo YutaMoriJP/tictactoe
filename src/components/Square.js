487887f559e00d5979f784d5275518f6ff569a29
Yuta Mori <5569823+YutaMoriJP@users.noreply.github.com>
2021-04-04 20:47:39 -0700

    fixed npm issues

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,6 +3,9 @@ import SquareStyled from "../styled/SquareStyled";
 
 const Square = ({ value, onClick, isOver, winningFormula, index }) => {
   const winner = isOver && winningFormula && winningFormula.includes(index);
+  console.log(winningFormula);
+  console.log(winner);
+  console.log(index);
   return (
     <SquareStyled x={/x/i.test(value) ? 1 : 0} winner={winner ? 1 : 0}>
       <h1 aria-label="button" onClick={onClick}>
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,12 +1,10 @@
 import React from "react";
 import GlobalStyle from "../../globalStyle/GlobalStyle";
-import CssBaseline from "@material-ui/core/CssBaseline";
 
 const Layout = ({ children }) => {
   return (
     <>
       <GlobalStyle />
-      <CssBaseline />
       <main>{children}</main>
     </>
   );
